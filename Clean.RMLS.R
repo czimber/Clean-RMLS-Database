4d98d0fa28782db307f5db0431f10d1bd350ac5d
czimber <craigzimber@gmail.com>
2014-06-06 19:25:21 -0700

    seperates good and bad rows

--- Clean.RMLS.R
+++ Clean.RMLS.R
@@ -2,14 +2,15 @@ w <- Weekly.RMLS.Download.Sample
 dim(w)
 #removes rows that dont start with a MLS#
 mls <- subset(w,w$Status == "ACT" | w$Status == "BMP" | w$Status == "CAN" | 
-                    w$Status == "EXP" | w$Status == "PEN" | w$Status == "SNL" | 
-                    w$Status == "SSP" | w$Status == "WTH" | w$Status == "SLD")
+                    w$Status == "EXP" | w$Status == "PEN" | w$Status == "SLD" | 
+                    w$Status == "SNL" | w$Status == "SSP" | w$Status == "WTH")
 dim(mls)
 
 # rows that dont start with a MLS#
-miss <- subset(w,w$Status != "ACT" & w$Status != "SLD" & w$Status != "BMP" & 
-                       w$Status != "CAN" & w$Status != "EXP" & 
-                       w$Status != "PEN" & w$Status != "SNL" & 
-                       w$Status != "SSP" & w$Status != "WTH" ) 
+miss <- subset(w,w$Status != "ACT" & w$Status != "BMP" & w$Status != "CAN" & 
+                      w$Status != "EXP" & w$Status != "PEN" & w$Status != "SLD" & 
+                      w$Status != "SNL" & w$Status != "SSP" & w$Status != "WTH")
+
 dim(miss)
+
 
